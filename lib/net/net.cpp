ecbfbfce84a036ca64f289b2b181f6174308db92
xiaokang00010 <zhouyongkang4321@gmail.com>
2025-11-02 08:36:00 +0800

    feat: port libnet from reXscript to hoshi-lang and revise CMakeLists.txt for OpenSSL

---
+++ lib/net/net.cpp
@@ -0,0 +1,156 @@
+#include "net.hpp"
+
+socketStartup::socketStartup() {
+    fdSet = std::make_shared<std::set<int>>();
+    sslConnectionSet = std::make_shared<std::set<sslInfo *>>();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+}
+
+socketStartup::~socketStartup() {
+    for (auto &i : *sslConnectionSet) {
+        SSL_CTX_free(i->ctx);
+        SSL_shutdown(i->ssl);
+        SSL_free(i->ssl);
+        delete i;
+    }
+    for (auto &i : *fdSet)
+        close_socket(i);
+    fdSet->clear();
+}
+
+socketStartup socketStartupInstance;
+
+LIBNET_EXPORT const char *libnet_resolve(const char *domain_name) {
+    // Set up hints for the getaddrinfo function
+    addrinfo hints{};
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;       // We want IPv4 addresses
+    hints.ai_socktype = SOCK_STREAM; // We want TCP sockets
+
+    // Call getaddrinfo to resolve the domain name
+    addrinfo *addr_list;
+    int error = getaddrinfo(domain_name, "80", &hints, &addr_list);
+    if (error != 0) {
+        throw std::runtime_error(std::string{"Failed to resolve domain name: "} + gai_strerror(error));
+    }
+
+    // Convert the resolved IP address to a string
+    char *ip_str = (char *)malloc(INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &((sockaddr_in *)addr_list->ai_addr)->sin_addr, ip_str, INET_ADDRSTRLEN);
+
+    // Free the memory allocated by getaddrinfo
+    freeaddrinfo(addr_list);
+
+    // Return the resolved IP address
+    return ip_str;
+}
+
+LIBNET_EXPORT int libnet_socket(int64_t *result) {
+    if (int rc = ::socket(AF_INET, SOCK_STREAM, 0); rc < 0) {
+        return rc;
+    } else {
+        socketStartupInstance.fdSet->insert(rc);
+        *result = rc;
+        return 0;
+    }
+}
+
+LIBNET_EXPORT int libnet_socket_connect(int fd, const char *ipAddr, uint16_t port) {
+    struct sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (int rc = inet_aton(ipAddr, &serv_addr.sin_addr); rc != 1) {
+        return LIBNET_INET_ATON_FAIL;
+    }
+    if (int rc = ::connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); rc < 0) {
+        return LIBNET_CONN_FAIL;
+    }
+    return 0;
+}
+
+LIBNET_EXPORT int libnet_socket_bind(int fd, const char *ipAddr, uint16_t port) {
+    struct sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (int rc = inet_aton(ipAddr, &serv_addr.sin_addr); rc != 1) {
+        return LIBNET_INET_ATON_FAIL;
+    }
+    if (int rc = ::bind(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); rc < 0) {
+        return rc;
+    }
+    return 0;
+}
+
+LIBNET_EXPORT int libnet_socket_accept(int fd, sockaddr_in *clientAddr, socklen_t *clientAddrLen) {
+    if (auto nfd = accept(fd, (struct sockaddr *)clientAddr, clientAddrLen); nfd > 0) {
+        socketStartupInstance.fdSet->insert(nfd);
+        return nfd;
+    } else {
+        return LIBNET_ACCEPT_FAIL;
+    }
+}
+
+LIBNET_EXPORT int libnet_socket_send(int fd, char *buf, u_int64_t len, int sendFlag) {
+    if (send(fd, buf, len, sendFlag) < 0) {
+        return LIBNET_SEND_FAIL;
+    }
+    return LIBNET_SUCCESS;
+}
+
+LIBNET_EXPORT int libnet_socket_recv(int fd, char *buf, u_int64_t len, int recvFlag) {
+    if (auto res = recv(fd, buf, len, recvFlag); res < 0) {
+        return LIBNET_RECV_FAIL;
+    } else if (res == 0) {
+        return 0;
+    } else {
+        return res;
+    }
+}
+
+LIBNET_EXPORT void libnet_socket_close(int fd) {
+    close_socket(fd);
+    socketStartupInstance.fdSet->erase(fd);
+}
+
+LIBNET_EXPORT sslInfo *libnet_ssl_connect(int fd) {
+    auto *r = new sslInfo();
+    if ((r->ctx = SSL_CTX_new(TLS_method())) == nullptr)
+        throw std::runtime_error("libnet: error creating ssl context");
+    r->ssl = SSL_new(r->ctx);
+    SSL_set_fd(r->ssl, fd);
+    if (auto retCode = SSL_connect(r->ssl); retCode != 1) {
+        throw std::runtime_error("libnet: error making ssl connection: ErrorCode: " +
+                                 std::to_string(SSL_get_error(r->ssl, retCode)));
+    }
+    r->fd = fd;
+    socketStartupInstance.sslConnectionSet->insert(r);
+    return r;
+}
+
+LIBNET_EXPORT int libnet_ssl_send(sslInfo *fd, char *data, u_int64_t len) {
+    if (SSL_write(fd->ssl, data, len) <= 0) {
+        return LIBNET_SSL_SEND_FAIL;
+    }
+    return LIBNET_SUCCESS;
+}
+
+LIBNET_EXPORT int libnet_ssl_recv(sslInfo *fd, char *dest, u_int64_t len) {
+    if (auto res = SSL_read(fd->ssl, dest, len); res < 0) {
+        return LIBNET_SSL_RECV_FAIL;
+    } else if (res == 0) {
+        return 0;
+    } else {
+        return res;
+    }
+}
+
+LIBNET_EXPORT void libnet_ssl_close(sslInfo *fd) {
+    SSL_CTX_free(fd->ctx);
+    SSL_shutdown(fd->ssl);
+    SSL_free(fd->ssl);
+    close_socket(fd->fd);
+    socketStartupInstance.fdSet->erase(fd->fd);
+    socketStartupInstance.sslConnectionSet->erase(fd);
+}
---
+++ lib/net/net.hpp
@@ -0,0 +1,117 @@
+//
+// Created by XIaokang00010 on 2025/11/02.
+//
+
+/**
+ * libnet: A Cross-Platform C++ Socket Binding Library for FFI
+ * Distributed along with hoshi-lang standard library under the MIT license.
+ * Copyright (c) 2025 Jerry Chou a.k.a XtherDevTeam
+ */
+
+#ifndef HOSHILANGSTD_LIBNET
+#define HOSHILANGSTD_LIBNET
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <netinet/in.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+#include <openssl/ssl.h>
+#include <set>
+#include <string>
+#include <unistd.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "libeay32.lib")
+#pragma comment(lib, "ssleay32.lib")
+
+struct __WSAINIT {
+    __WSAINIT() {
+        WSADATA wsa_data;
+        WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    }
+
+    ~__WSAINIT() {
+        WSACleanup();
+    }
+} ___WSAINIT;
+
+#define inet_aton(a, b) inet_pton(AF_INET, (a), (b))
+#define close_socket closesocket
+#else
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define close_socket close
+
+#endif
+
+#define LIBNET_EXPORT extern "C"
+
+#define LIBNET_SUCCESS 0
+#define LIBNET_INET_ATON_FAIL -1
+#define LIBNET_CONN_FAIL -2
+#define LIBNET_LISTEN_FAIL -3
+#define LIBNET_ACCEPT_FAIL -4
+#define LIBNET_SEND_FAIL -5
+#define LIBNET_RECV_FAIL -6
+#define LIBNET_SSL_CONNECT_FAIL -7
+#define LIBNET_SSL_SEND_FAIL -8
+#define LIBNET_SSL_RECV_FAIL -9
+
+
+#define LIBNET_DEFAULT_BACKLOG 5
+
+struct sslInfo {
+    SSL *ssl{};
+    SSL_CTX *ctx{};
+    int fd{};
+};
+
+struct socketStartup {
+    std::shared_ptr<std::set<int>> fdSet;
+    std::shared_ptr<std::set<sslInfo *>> sslConnectionSet;
+
+    socketStartup();
+
+    ~socketStartup();
+};
+
+extern socketStartup socketStartupInstance;
+
+// Parse the given domain name and return the resolved IP address as a string
+// pointer shall be finalized by the caller
+LIBNET_EXPORT const char *libnet_resolve(const char *domain_name);
+
+LIBNET_EXPORT int libnet_socket(int64_t *result);
+
+LIBNET_EXPORT int libnet_socket_connect(int fd, const char *ipAddr, uint16_t port);
+
+LIBNET_EXPORT int libnet_socket_bind(int fd, const char *ipAddr, uint16_t port);
+
+LIBNET_EXPORT int libnet_socket_listen(int fd, int backlog = LIBNET_DEFAULT_BACKLOG);
+
+LIBNET_EXPORT int libnet_socket_accept(int fd, sockaddr_in *clientAddr, socklen_t *clientAddrLen);
+
+LIBNET_EXPORT int libnet_socket_send(int fd, char *buf, u_int64_t len, int sendFlag = 0);
+
+LIBNET_EXPORT int libnet_socket_recv(int fd, char *buf, u_int64_t len, int recvFlag = 0);
+
+LIBNET_EXPORT void libnet_socket_close(int fd);
+
+LIBNET_EXPORT sslInfo *libnet_ssl_connect(int fd);
+
+LIBNET_EXPORT int libnet_ssl_send(sslInfo *fd, char *data, u_int64_t len);
+
+LIBNET_EXPORT int libnet_ssl_recv(sslInfo *fd, char *dest, u_int64_t len);
+
+LIBNET_EXPORT void libnet_ssl_close(sslInfo *fd);
+
+#endif // HOSHILANGSTD_LIBNET
