e22aa66e70913f997601b8dd1f55b77c36e727ba
xiaokang00010 <zhouyongkang4321@gmail.com>
2025-12-31 22:06:39 +0800

    feat: initiate development of pardo-server

---
+++ pardo-server/share/scheme.sql
@@ -0,0 +1,35 @@
+create table config (
+    host            string primary key default 'localhost',
+    port            integer not null default 8080,
+    artifacts_dir   string not null default 'blob/artifacts'
+);
+
+create table users (
+    id              integer primary key,
+    username        string not null,
+    password_salted string not null,
+    email           string not null default '',
+    description     string not null default '',
+    permission      integer not null default 1
+);
+
+create table packages (
+    id              integer primary key,
+    name            string not null,
+    author          string not null,
+    description     string not null,
+    repository      string not null,
+    license         string not null,
+    versions        string default '[]'
+);
+
+create table artifacts (
+    id              integer primary key,
+    package_id      integer not null,
+    version         string not null,
+    filename         string not null,
+    mime            string not null,
+    destinations    string not null
+);
+
+insert into config (host, port, artifacts_dir) values ('localhost', 8080, 'blob/artifacts');
